1292639d0291b7c878ffb76d41a6c349e722405f
PharaohKJ <kato@phalanxware.com>
2017-08-09 22:26:59 +0900

    MQTT対応

--- main.rb
+++ main.rb
@@ -4,21 +4,35 @@ require 'rubygems'
 gem 'serialport','>=1.0.4'
 require 'serialport'
 require 'logger'
+require 'mqtt'
 
 log = Logger.new('/tmp/wc_log.txt')
 
-sp = SerialPort.new('/dev/ttyUSB0', 115200, 8, 1, 0) # 115200, 8bit, stopbit 1, parity none
+sp = SerialPort.new('/dev/ttyUSB0', 115_200, 8, 1, 0) # 115200, 8bit, stopbit 1, parity none
+
+@client = MQTT::Client.connect(host: 'a1f18lql3l5z0z.iot.ap-northeast-1.amazonaws.com',
+                               port: 8883,
+                               ssl: true,
+                               cert_file: 'cert.pem',
+                               key_file: 'private-key.pem',
+                               ca_file: 'rootCA.pem')
+
 wc = Wc.new
 loop do
   begin
     line = sp.gets # read
     data = TweliteSerialData.new(line.strip)
     wc.add(data) do |x, v|
-      puts x, v
+      log.info "event value = #{x}, value = #{v}"
+      value_map = {
+        1 => 'IN',
+        -1 => 'OUT'
+      }
+      @client.publish('wc/status', value_map[x]) unless value_map[x].nil?
     end
-    # log.info data[:analog_in1]
-    # log.info data.original
+    log.info data[:analog_in1]
+    log.info data.original
   rescue => e
-    log.fatal "[ERROR] #{e.to_s}"
+    log.fatal e.to_s
   end
 end
